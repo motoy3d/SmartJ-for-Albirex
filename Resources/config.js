13c9a3000e74b467ca81acc23448b56c354c4f0e
motoy3d <motoy3d@gmail.com>
2014-12-10 01:26:14 +0900

    first

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'albirex'
+    ,hashtag: 'albirex'
+    ,teamName: '新潟'
+    ,appName: 'スマートJ for アルビレックス'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id905888676?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=everyday.albirex'
+    ,color: '#f57b00'
+    ,tintColor: 'white'
+    ,dbName: 'everyday.albirex'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=albirex&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=albirex"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 212662
+    ,nendApiKeyIPhoneIcon: '048bd97189a096e74a0c4a55fea3db221dacd075'
+    ,nendSpotIdIPhoneBanner: 216920
+    ,nendApiKeyIPhoneBanner: '0959f05ed1d3aaff80844d40915808458a29cabd'
+    //nend(Android)
+    ,nendSpotIdAndroid: 213649
+    ,nendApiKeyAndroid: '4ea3ad2420188f0d0be9f2c3d9f290bf26abd546'
+};

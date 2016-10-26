3e915700d0f802d38e78becb1fc08f33b4e9ea54
motoy3d <motoy3d@gmail.com>
2016-10-26 14:28:24 +0900

    ver1.1.1

--- Resources/config.js
+++ Resources/config.js
@@ -2,18 +2,40 @@ exports.config = {
     teamId: 'albirex'
     ,hashtag: 'albirex'
     ,teamName: '新潟'
+    ,teamNameFull: 'アルビレックス新潟'
     ,appName: 'スマートJ for アルビレックス'
     ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id905888676?l=ja&ls=1&mt=8'
     ,androidAppUrl: 'https://play.google.com/store/apps/details?id=everyday.albirex'
+    ,backgroundColor: 'black'
+	,visitedBgColor : '#457'
+    ,mainTextColor: 'white'
     ,color: '#f57b00'
     ,tintColor: 'white'
+    ,standingsBgcolor: '#f57b00'
+    ,standingsTeamFontColor: 'white'
+    ,resultsDetailBtnBgColorActive: '#333'
+    ,resultsDetailBtnColorActive: 'white'
+    ,resultsDetailBtnBgColorInactive: '#202020'
+    ,resultsDetailBtnColorInactive: '#444'
     ,dbName: 'everyday.albirex'
+    //URLベース
+    ,urlBase: 'https://smartj.mobi/redsmylife/'
     //ニュースのソースURL
     ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
     //日程・結果のソースURL
     ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=albirex&season="
+    //順位表のソースURL
+    ,standingsUrl: 'http://sub0000499082.hmk-temp.com/redsmylife/standings.json'
     //設定、メッセージのソースURL
     ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=albirex"
+    //動画のソースURL
+    ,videoUrl: "http://sub0000499082.hmk-temp.com/redsmylife/video.json?teamId=albirex"
+    //報告URL
+    ,reportUrl: "https://smartj.mobi/redsmylife/report.json?teamId=albirex"
+    //利用規約URL
+    ,rulesUrl: "https://smartj.mobi/redsmylife/rules.jsp?appName="
+    //開発元メールアドレス
+    ,developerMail: "motoy3d+smartj@gmail.com"
     ,newsEntriesPerPage: 50
     ,googleAnalyticsTrackingId: 'UA-30928840-1'
     //nend(iPhone)
@@ -24,4 +46,6 @@ exports.config = {
     //nend(Android)
     ,nendSpotIdAndroid: 213649
     ,nendApiKeyAndroid: '4ea3ad2420188f0d0be9f2c3d9f290bf26abd546'
+    ,nendSpotIdAndroidBanner: 393946
+    ,nendApiKeyAndroidBanner: '91d3db19950eec754416a5d2604b1b60915b7bb8'
 };
